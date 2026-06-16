1259f9fcdb39b7683f3fd2f0ef9addcdda5c03bf
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-16 18:40:30 +0530

    Declare offset and group id

--- stock-kafka-pipeline/consumer_dashboard.py
+++ stock-kafka-pipeline/consumer_dashboard.py
@@ -1,10 +1,14 @@
 from kafka import KafkaConsumer
 import json
 from config import KAFKA_BROKER, KAFKA_TOPIC
+
+def deserializer(v):
+	return json.loads(v.decode('utf-8'))
  
 
 consumer=KafkaConsumer(
 	KAFKA_TOPIC,
 	bootstrap_servers=KAFKA_BROKER,
 	group_id='dashboard-group',
+	auto_offset_reset='earliest'
 	) 
