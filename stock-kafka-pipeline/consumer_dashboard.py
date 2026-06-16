fef3961a201c4135a2659e6ec093e0f4f4e62c1b
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-16 18:25:52 +0530

    create consumer variable

--- stock-kafka-pipeline/consumer_dashboard.py
+++ stock-kafka-pipeline/consumer_dashboard.py
@@ -1,3 +1,10 @@
 from kafka import KafkaConsumer
 import json
 from config import KAFKA_BROKER, KAFKA_TOPIC
+ 
+
+consumer=KafkaConsumer(
+	KAFKA_TOPIC,
+	bootstrap_servers=KAFKA_BROKER,
+	group_id='dashboard-group',
+	) 
