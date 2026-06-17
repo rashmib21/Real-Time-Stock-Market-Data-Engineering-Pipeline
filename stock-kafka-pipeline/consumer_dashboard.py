d1932cbcfe8c518600d0f81799dd3755420dc42f
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-17 12:34:22 +0530

    Create a loop for displaying the real-tim open, high, low, close and volume in the consumer dashboard

--- stock-kafka-pipeline/consumer_dashboard.py
+++ stock-kafka-pipeline/consumer_dashboard.py
@@ -6,9 +6,32 @@ def deserializer(v):
 	return json.loads(v.decode('utf-8'))
  
 
-consumer=KafkaConsumer(
-	KAFKA_TOPIC,
-	bootstrap_servers=KAFKA_BROKER,
-	group_id='dashboard-group',
-	auto_offset_reset='earliest'
-	) 
+consumer=KafkaConsumer(  #This creates consumer object and immediately connects it to Kafka
+	KAFKA_TOPIC, #Topic name same as producer topic write in it 
+	bootstrap_servers=KAFKA_BROKER, 
+	group_id='dashboard-group', #different consumer groups it is of dashboard
+	value_deserializer=deserializer,   
+	auto_offset_reset='earliest' #position in the topic, earliest means: if the consumer has never run 
+	#before and has no saved position, start reading from the very first message which stored in the topic, if we write latest it will read only new messages
+		) 
+
+print("Dashboard consumer started. Waiting for data...")
+print("-"*50)
+
+for message in consumer:
+	data=message.value
+	# print(type(data))
+	# print(data['symbol'])
+
+	print(f"Symbol: {data['symbol']}")
+	print(f"LTP: Rs.{data['ltp']:2f}")
+	print(f"Open: Rs.{data['open']:2f}")
+	print(f"High: Rs.{data['high']:2f}")
+	print(f"Low: Rs.{data['low']:2f}")
+	print(f"Close: Rs.{data['close']:2f}")
+	print(f"Volume: Rs.{data['volume']:2f}")
+	print(f"Time: {data['timestamp']}")
+	print("-"*50)	
+
+
+
