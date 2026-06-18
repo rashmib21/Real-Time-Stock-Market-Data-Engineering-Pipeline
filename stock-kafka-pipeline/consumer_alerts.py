8f832e0563f0a914e65e70a4bd465b929ca3b6c0
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-18 13:05:05 +0530

    create consumer variable to add config

--- stock-kafka-pipeline/consumer_alerts.py
+++ stock-kafka-pipeline/consumer_alerts.py
@@ -1,3 +1,11 @@
 from kafka import KafkaConsumer
 import json
 from config import *
+
+consumer=KafkaConsumer(
+	KAFKA_TOPIC,
+	bootstrap_servers=KAFKA_BROKER,
+	group_id='alerts-group',
+	value_deserializer=deserializer,
+	auto_offset_reset='earliest'
+	)
