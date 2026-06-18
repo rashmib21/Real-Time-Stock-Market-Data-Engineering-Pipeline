9b7f148997b6cda3272723883c231b4db1b7d231
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-18 12:19:05 +0530

    Add comments in consumer_analytics

--- stock-kafka-pipeline/consumer_analytics.py
+++ stock-kafka-pipeline/consumer_analytics.py
@@ -1,5 +1,5 @@
 from kafka import KafkaConsumer
-from collections import deque
+from collections import deque  #deque is a special list which removes the old item when it gets full
 import json
 from config import KAFKA_BROKER, KAFKA_TOPIC
 
@@ -9,8 +9,8 @@ def deserializer(v):
 consumer=KafkaConsumer(
 	KAFKA_TOPIC,
 	bootstrap_servers=KAFKA_BROKER,
-	group_id='analytics-group',
+	group_id='analytics-group', #different group id for analytics instead of dashboard
 	value_deserializer=deserializer,
 	auto_offset_reset='earliest'
 	)
-price_window=deque(maxlen=5)
+price_window=deque(maxlen=5) #it creates an empty deque which can hold at most 5 items. When the 6th item is added the first old one is removed 
