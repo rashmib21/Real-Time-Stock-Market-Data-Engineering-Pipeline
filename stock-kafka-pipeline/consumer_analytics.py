2236208d93dc86cffa9cc4ce65b1e54f461883dc
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-18 12:07:25 +0530

    Create deserializer function

--- stock-kafka-pipeline/consumer_analytics.py
+++ stock-kafka-pipeline/consumer_analytics.py
@@ -3,6 +3,9 @@ from collections import deque
 import json
 from config import KAFKA_BROKER, KAFKA_TOPIC
 
+def deserializer(v):
+	return json.loads(v.decode('utf-8'))
+
 consumer=KafkaConsumer(
 	KAFKA_TOPIC,
 	bootstrap_servers=KAFKA_BROKER,
