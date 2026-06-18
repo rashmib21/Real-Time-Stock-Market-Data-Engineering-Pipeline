5d070f29f9bfec09144f85ecda5655f8acf120cd
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-18 13:02:18 +0530

    import libraries in consumer_alerts

--- stock-kafka-pipeline/consumer_analytics.py
+++ stock-kafka-pipeline/consumer_analytics.py
@@ -22,12 +22,12 @@ for message in consumer:
 	# print(data)
 	ltp=data['ltp']
 
-	price_window.append(ltp)
+	price_window.append(ltp) #append() adds the latest closing price to the right end of the deque
 
 	if len(price_window)==5:
 		sma=sum(price_window)/len(price_window)  #Simple moving average
 		print(f"[ANALYTICS] {data['symbol']}")
-		print(f"Last 5 closes: {list(price_window)}")
+		print(f"Last 5 closes: {list(price_window)}") #Converts the deque to a regular Python list just for printing
 		print(f"5-period SMA: Rs. {sma:.2f}")
 	else:
 		remaining=5-len(price_window)
