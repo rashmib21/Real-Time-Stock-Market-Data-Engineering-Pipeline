5202e88187ff0f1fe85d79a72297875840e76bd1
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-18 14:30:28 +0530

    consumer_storage is complete

--- stock-kafka-pipeline/consumer_storage.py
+++ stock-kafka-pipeline/consumer_storage.py
@@ -24,3 +24,28 @@ consumer=KafkaConsumer(
 
 print('Storage consumer started. Writing to MYSQL...')
 
+INSERT_SQL="""INSERT INTO stock_events(symbol, ltp, open_price, high_price, low_price, close_price, volume, event_timestamp) VALUES 
+			(%s, %s, %s, %s, %s, %s, %s, %s)"""
+
+for message in consumer:
+	data=message.value
+
+	values=(
+		data['symbol'],
+		data['ltp'],
+		data['open'],
+		data['high'],
+		data['low'],
+		data['close'],
+		data['volume'],
+		data['timestamp']
+		)		
+
+		try: 
+			cursor.execute(INSERT_SQL, values)
+			conn.commit()
+			print(f"[STORAGE] Saved: {d['symbol']} | LTP={d['ltp:.2f']}")
+
+		except Exception as e:
+			print(f"[STORAGE] DB error: {e}")
+			conn.rollback()	
