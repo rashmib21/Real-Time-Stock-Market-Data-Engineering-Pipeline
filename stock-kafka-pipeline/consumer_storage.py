6ae56e5bf824078bd0b62f6bbaf1dea741e845ac
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-18 14:13:17 +0530

    add credentials of database

--- stock-kafka-pipeline/consumer_storage.py
+++ stock-kafka-pipeline/consumer_storage.py
@@ -2,3 +2,10 @@ from kafka import KafkaConsumer
 import mysql.connector
 import json
 from config import *
+
+conn=mysql.connnector.connect(
+	host='MYSQL_HOST',
+	user='MYSQL_USER',
+	password='MYSQL_PASS',
+	database=MYSQL_DB
+	)
