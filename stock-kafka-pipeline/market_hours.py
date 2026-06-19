b7204e5b76a9819568c04d6dc91d3c1a80fe6e64
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-19 16:56:57 +0530

    Resolve merge conflicts

--- stock-kafka-pipeline/market_hours.py
+++ stock-kafka-pipeline/market_hours.py
@@ -3,7 +3,7 @@ import datetime
 def is_market_open():
 	now=datetime.datetime.now()
 
-	if now.weekday()>=5
+	if now.weekday()>=5:
 		return False
 
 	market_open=now.replace(hour=9, minute=15, second=0, microsecond=0)
