3088ff859debfa8ad3fcd96fd4921cd9a6aa5ec9
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-19 14:43:27 +0530

    return the function

--- stock-kafka-pipeline/market_hours.py
+++ stock-kafka-pipeline/market_hours.py
@@ -9,4 +9,4 @@ def is_market_open():
 	market_open=now.replace(hour=9, minute=15, second=0, microsecond=0)
 	market_close=now.replace(hour=9, minute=30, second=0, microsecond=0)
 
-		
+	return market_open<= now<=market_close	
